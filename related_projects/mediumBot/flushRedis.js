60b2d495c17a6eb3ea90fb6f07e6eeb18df51087
mlibre <m.gh@linuxmail.org>
2018-05-17 00:34:41 +0430

    basic telegram bot. ready for test

--- related_projects/mediumBot/flushRedis.js
+++
@@ -1,9 +0,0 @@
-let redis = require('redis');
-let client = redis.createClient();
-client.select(3);
-client.on('error' , function (err)
-{
-	console.error(new Error(`#Error. message: ${err}`.red));
-});
-client.flushall();
-client.quit();
